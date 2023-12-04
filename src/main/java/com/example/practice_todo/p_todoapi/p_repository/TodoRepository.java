c108f817b72ebd3af198f961dcbce5ecbdd90c22
hyeonseon <hsuncho@naver.com>
2023-12-04 15:11:32 +0900

    #10. 카카오 로그인 진행 중(인가코드, access token 처리)

--- src/main/java/com/example/practice_todo/p_todoapi/p_repository/TodoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.practice_todo.p_todoapi.p_repository;
-
-import com.example.practice_todo.p_todoapi.p_entity.Todo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TodoRepository
-        extends JpaRepository<Todo,String> {
-}
